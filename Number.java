e92ca4a679dd8b7ab38aef34b220a869e40c3a7c
Ajay <ajay595uk@gmail.com>
2018-05-25 10:43:01 +0530

    Number.java v1

---
+++ Number.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+public class Number{
+	public static void main (String args[]){
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter number");
+		int num=s.nextInt();
+		if(num%2==0){
+			System.out.println("Number is Even");
+		}
+		else{
+			System.out.println("Number is Odd");
+		}	
+	}
+}
